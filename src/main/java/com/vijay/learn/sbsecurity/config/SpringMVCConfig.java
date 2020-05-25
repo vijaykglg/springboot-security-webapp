9d7d60a24c4cbd4d735f505bf8d1bcdd57683be0
vijaykglg <vijay.gupta7@yahoo.in>
2020-05-25 12:29:18 +0530

    Unsecured Web Application-Application without Spring Security

---
+++ src/main/java/com/vijay/learn/sbsecurity/config/SpringMVCConfig.java
@@ -0,0 +1,20 @@
+package com.vijay.learn.sbsecurity.config;
+/*
+Project : springboot-security-webapp
+User    : Vijay Gupta
+Date    : May 2020
+*/
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMVCConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
