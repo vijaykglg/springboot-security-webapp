aac52b4230b19559fbc63f216853d68cf261fb25
vijaykglg <vijay.gupta7@yahoo.in>
2020-05-25 11:48:15 +0530

    Initial Commit without Spring Security

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spring Certification Training
+
+##Securing a Web Application
+This guide walks you through the process of creating a simple web application with resources that are protected by Spring Security.
---
+++ src/test/java/com/vijay/learn/sbsecurity/SpringbootSecurityWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.learn.sbsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
